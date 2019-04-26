3d34b1535b9b414c5fbcd8da7fa33fff4f148476
Ruben Van Assche <ruben@spatie.be>
2019-04-26 10:30:12 +0200

    wip

--- src/GoogleTimeZoneServiceProvider.php
+++ src/GoogleTimeZoneServiceProvider.php
@@ -10,17 +10,19 @@ final class GoogleTimeZoneServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/google-time-zone.php' => config_path('google-time-zone.php'),
+            __DIR__ . '/../config/google-time-zone.php' => config_path('google-time-zone.php'),
         ], 'config');
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/google-time-zone.php', 'google-time-zone');
+        $this->mergeConfigFrom(__DIR__ . '/../config/google-time-zone.php', 'google-time-zone');
 
         $this->app->bind('google-time-zone', function ($app) {
             return (new GoogleTimeZone(new Client))
-                ->setApiKey(config('google-time-zone.key'));
+                ->setApiKey(config('google-time-zone.key'))
+                ->setLanguage(config('google-time-zone.language', 'language'));
+
         });
     }
 }
